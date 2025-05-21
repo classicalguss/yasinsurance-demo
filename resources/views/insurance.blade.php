809b5e0cb5d961d5a9a8786b3aca3aac725f074e
Ghassan Barghouti <ghassan@yasmina.ai>
2025-05-21 10:28:04 +0300

    team layout

--- resources/views/insurance.blade.php
+++ resources/views/insurance.blade.php
@@ -1,3 +1,4 @@
+<x-layouts.app :title="__('team')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <div class="mt-6 ms-2">
@@ -13,3 +14,4 @@
 
         </div>
     </div>
+</x-layouts.app>
