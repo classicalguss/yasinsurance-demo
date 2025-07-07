4636c264f6415eb11239a57838b68294f657dc7e
Ghassan Barghouti <ghassan@yasmina.ai>
2025-07-07 10:44:01 +0300

    webhook

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 
 Route::post('/test-webhook', function () {
+	logger(request()->headers);
+	logger(request()->all());
 	return response()->json([
 		'message' => 'Webhook received successfully',
 		'received_data' => request()->all(),
